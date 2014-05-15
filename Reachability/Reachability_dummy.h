b6a124327145d7c9c026460b5cdb07f754c52f7b
Roth Michaels <roth@rothmichaels.us>
2014-05-15 11:57:56 -0400

    Setup dummy Reachability project.

---
+++ Reachability/Reachability_dummy.h
@@ -0,0 +1,13 @@
+//
+//  Reachability.h
+//  Reachability
+//
+//  Created by Roth Michaels on 5/15/14.
+//  Copyright (c) 2014 Roth Michaels. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Reachability_dummy : NSObject
+
+@end
---
+++ Reachability/Reachability_dummy.m
@@ -0,0 +1,13 @@
+//
+//  Reachability.m
+//  Reachability
+//
+//  Created by Roth Michaels on 5/15/14.
+//  Copyright (c) 2014 Roth Michaels. All rights reserved.
+//
+
+#import "Reachability_dummy.h"
+
+@implementation Reachability_dummy
+
+@end
